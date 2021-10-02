b91b489c73cf2f5589eb19d62ad3803cf892d3ce
Bharat Rathi <rathibharat27412@gmail.com>
2021-10-02 15:09:18 +0530

    initial commit with auth

---
+++ befit/Login/views.py
@@ -0,0 +1,40 @@
+from django.core.checks import messages
+from django.http.request import QueryDict
+from django.shortcuts import redirect, render
+from .models import todo
+from django.contrib.auth import authenticate , login, logout
+from django.contrib.auth.models import User
+from .forms import CreateUserForm
+
+
+def Signup_view(request):
+    if 'email_signup' in request.POST:
+        username = request.POST['username_signup']
+        email = request.POST['email_signup']
+        pass1 = request.POST['psw_signup1']
+        pass2 = request.POST['psw_signup2']
+
+        if pass1 != pass2:
+            return render(request,'Login.html')
+
+        myuser = User.objects.create_user(username=username,email=email,password = pass1)
+        myuser.save()
+        return redirect('/')
+
+    elif 'email' in request.POST:
+        loginusername = request.POST['email']
+        loginpassword = request.POST['psw']
+
+        user = authenticate(username = loginusername,password = loginpassword)
+        
+        if user is not None:
+            print('inside')
+            login(request,user)
+            print('successful')
+            return redirect('/')
+        else:
+            print('failed')
+            return redirect('/')
+
+    return render(request,'Login.html')
+
